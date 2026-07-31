2cf965eb2a4ff2681062fc2e6ade5b18d79b1ef1
Pratyush Kumar <kumarkinshuk14@gmail.com>
2026-07-30 22:30:45 -0700

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    int m, n, l;
+    vector<vector<int>> directions {{0,1}, {0,-1}, {1,0}, {-1,0}};
+private:
+    bool find(vector<vector<char>>& board, int i, int j, string& word, int index) {
+        if (index >= word.size()) return true;
+
+        if (i < 0 || j < 0 || i >=m || j >=n || board[i][j] != word[index]) return false;
+
+        char temp = board[i][j];
+        board[i][j] = ' ';
+        for(auto& direction : directions) {
+            int i_ = i + direction[0];
+            int j_ = j + direction[1];
+
+            if (find(board, i_, j_, word, index+1))
+                return true;
+        }
+
+        board[i][j] = temp;
+        return false;
+    }
+
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size();
+        n = board[0].size();
+        l = word.size();
+
+        if(m*n < l) return false;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (board[i][j] == word[0] && find(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
